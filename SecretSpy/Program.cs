9061aceb5c9c0de881e880339d04637271333f56
Santiago Figueroa <santiagofigueroa@capacitas.co.uk>
2019-06-10 13:41:26 +0100

    First commit for SpyMessage

---
+++ SecretSpy/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace SecretSpy
+{
+
+    class Program
+    {
+        public static int Message { get; set; }
+        public static int Spy { get; set; }
+
+
+        static int Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Spy Program");
+            int Message = 0;
+            int[] array = new int[8];
+            Console.WriteLine("Please Enter Message !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            Console.WriteLine();
+
+            while (Message < 9)
+            {
+                if (int.TryParse(Console.ReadLine(), out array[Message]))
+                {
+                    Message++;
+                }
+                else
+                {
+                    Console.WriteLine("You didn't enter a number! Please enter again!");
+                }
+                    
+            }
+
+            //Message = Console.Read();
+            //if (array.Length > 0 )
+            //{
+            //    object obj = new object();
+
+            //    Console.WriteLine("Please enter a message");  
+            //} 
+            //if (!Validate) { }
+            //for (int i = 0; i < Message; i++)
+            //{
+            //    array[i] = int.Parse(Console.ReadLine());
+            //    Console.WriteLine(array[i]);
+            //}
+            int spyIndex = 0;
+            int[] spy = new int[3];
+            Console.WriteLine("Please Enter Spy Number");
+
+            while (spyIndex < 4)
+            {
+                if (int.TryParse(Console.ReadLine(), out spy[spyIndex]))
+                    spyIndex++;
+                else
+                    Console.WriteLine("You didn't enter a number! Please enter again!");
+            }
+            Console.Read();
+
+            return 0;
+        }
+    }
+}
---
+++ SecretSpy/Stack.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSpy
+{
+    /// <summary>
+    /// Stack Class
+    /// This class will be use to find the sequence of [0,0,7] and [3,1,4]
+    /// </summary>
+   public class Stack
+    {
+        private int maxSize;
+        private int[] stackArray;
+        private int top;
+
+        public Stack(int size)
+        {
+            maxSize = size;
+            stackArray = new int[maxSize];
+            top = - 1; 
+        }   
+        
+        public void push(int pushIntElement)
+        {
+            if (isFull())
+            {
+
+            }
+            else
+            {
+                top++;
+                stackArray[top] = pushIntElement;
+            }
+        }
+
+        private int pop()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Is Empty");
+                return -1; 
+            }else {
+                // do copy of old_top make the top Minus.  
+                int old_top = top;
+                top--;
+                return stackArray[old_top];
+
+            }
+        }
+
+        private bool isEmpty()
+        {
+            return (top == -1); 
+        }
+
+        private bool isFull()
+        {
+            // -1 becoause that's how we started in the constructor.
+            return (maxSize - 1 == top); 
+        }
+    }
+}
